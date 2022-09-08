74bb0f7c41de49a325b3957464779a1547ab03aa
New Pagodi <NewPagodi@users.noreply.github.com>
2022-09-08 15:13:55 -0500

    Add to readme and created src files

---
+++ src/main/java/com/revature/po/codechallenge/LargestArrayGap.java
@@ -0,0 +1,13 @@
+package com.revature.po.codechallenge;
+
+public class LargestArrayGap {
+	private final int[] arr;
+	
+	LargestArrayGap(int[] arr) {
+		this.arr = arr;
+	}
+	
+	public int findLargestGap() {
+		return 0;
+	}
+}
---
+++ src/main/java/com/revature/po/codechallenge/MaximumSumSubArray.java
@@ -0,0 +1,18 @@
+package com.revature.po.codechallenge;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MaximumSumSubArray {
+	final private int[] arr;
+	
+	MaximumSumSubArray(int[] arr) {
+		this.arr = arr;
+	}
+	
+	public List<int[]> getMaxSumSubArray() {
+		List<int[]> maxs = new LinkedList<>();
+		
+		return maxs;
+	}
+}
+
